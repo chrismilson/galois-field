4de588ba29e2463f24c5c2e5baed8be7f57c0a84
chrismilson <chris.w.milson@gmail.com>
2016-08-09 20:14:47 +1200

    Update README and testing functionality

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # galois-field
-A class for representing and manipulating Galois Fields (of prime order so far)
+A class for representing and manipulating Galois Fields (of prime order so far).
+
+The purpose of this project is to be a support class for manipulating codes for error correcting codes over finite fields.
+
+## Install
+
+Just do make to get the object file, however the recommended use is to take the source and use it like a library in other programs.
+
+Modify the test.cpp file to play around with, it alrady has some explanation of the functions but feel free to change it.
--- test.cpp
+++ test.cpp
@@ -3,10 +3,33 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  Field test1(53);
-  test1 = 7;
+  Field test1(7);
+  test1 = 3;
 
-  cout << "test1 = " << test1 * test1 << endl;
+  cout << "This is an example of a finite field of order 7." << endl;
+  cout << "In GF(7), 3 + 3 = " << test1 + test1 << ", ";
+  cout << "-3 = " << -test1 << ", ";
+  cout << "and (the multiplicative inverse of 3) 1 / 3 = " << test1.inv() << endl;
+
+  cout << endl;
+
+  cout << "Be careful when trying to work with Fields of different order!" << endl;
+  try {
+    Field test2(5), test3(11);
+    Field test4 = test2 + test3;
+  } catch (IncompatibleField e) {
+    cout << "Fields must be of the same order to operate on the elements in them." << endl;
+  }
+
+  cout << endl;
+
+  cout << "Also, be careful when constructing Fields of non-prime order." << endl;
+  cout << "There are only finite fields of order p^q where p is prime and q is a natural number." << endl;
+  try {
+    Field nonPrime(9);
+  } catch (InvalidFieldOrder e) {
+    cout << "In this implementation however, we can only deal with fields of PRIME order." << endl;
+  }
 
   return 0;
 }
